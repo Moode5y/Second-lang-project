da43eeeab41dabf2b7d594f39ade420c8a1bdae8
MOHAMED <f6ham5d@gmail.com>
2024-06-19 16:21:39 +0300

    add some lessons

--- main.js
+++ main.js
@@ -17,3 +17,85 @@ window.onload = function () {
     5
   */
   // Ctrl + /
+
+
+  /*
+  Output To Screen
+  - window.alert()
+  - document.write()
+  - console.log()
+
+  %c
+  Syntax
+*/
+console.log("hello %c im %c mohamed", "color: blue; font-size: 25px","color: #5daa41; font-size: 25px;" );
+
+document.write("<h1>Hello <span>Page</span></h1>");
+
+document.write("Hi Guys")
+console.log("Hello This is My project");
+
+// window - document - console
+
+/* 
+- console methods
+log( )
+error( )
+table(["osama", "khaled", "omar"]); 
+....etc
+*/
+
+// ===============================================================================
+
+/*
+Data Types intro 
+- String 
+- Numbers
+- Array => Object
+- Object
+- Boolran 
+*/
+
+console.log(typeof "Mohamed Ahmed"); // This is String
+console.log(typeof 5000); // This is Number
+console.log(typeof 5000.99); // This is Number
+console.log(typeof [10, 50, 60]); // This Is array OR Object
+console.log({ name: "Mohamed", age: 15, place: "jeddah" }); // this is Object
+console.log(typeof true); // boolean
+console.log( typeof undefined);
+console.log(typeof null) // Object
+
+// =================================Varibales===================================
+
+/*
+var + let + const = varibales
+syntax (keyword | variabe name | Assignment operator | value )
+camalCase
+
+=========== The diffrent between Varibales ========
+var 
+- Redeclare (Yes)
+- Access Before Declare (Yes)
+- Varibale scope drama [Added To window]
+- Block Or Scope Function
+
+let
+- Redeclare (no => Error)
+- Access Before Declare (Error)
+- Varibale scope drama []
+- Block Or Scope Function
+
+const
+- Redeclare (No => Error)
+- Access Before Declare (Error)
+- Varibale scope drama []
+- Block Or Scope Function
+*/
+
+console.log(name);
+var name = "mohamed";
+
+let user = 15;
+console.log(user);
+
+// =================================String=====================================
