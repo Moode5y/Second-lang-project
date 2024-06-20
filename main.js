56d403ddfdbfcb03f4e5141554ca16863077e6a0
MOHAMED <f6ham5d@gmail.com>
2024-06-20 06:27:22 +0300

    add some lessons

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ console.log("hello %c im %c mohamed", "color: blue; font-size: 25px","color: #5d
 
 document.write("<h1>Hello <span>Page</span></h1>");
 
-document.write("Hi Guys")
+// document.write("Hi Guys\n")
 console.log("Hello This is My project");
 
 // window - document - console
@@ -99,3 +99,64 @@ let user = 15;
 console.log(user);
 
 // =================================String=====================================
+/*
+String Syntax 
+Character Escape Sequences \\For Search
+\ Escape 
+Line Continue
+\n
+*/
+
+console.log('hello my "boy"');
+console.log("hello my 'boy'");
+console.log("hello my \"boy\"");
+console.log("hello \\my boy");
+console.log("hello\
+my\
+boy");
+console.log("hello\nmy\nboy");
+
+// Concatenation
+// Template Literals (Template String)
+
+let a = "I";
+let b = "Love";
+let c = "You";
+
+console.log(a, b);
+document.write(a + " " + b + " " + c); // Old Way
+
+console.log(` ${a} "
+  ${b} " 
+  \\ ${c} `);
+
+let markup = `
+  <div class="card"
+    <div class="child"
+      <h2>${a} ${b} ${c} </h2>
+    </div>
+  </div>
+`
+document.write(markup)
+
+/*
+===========================================
+== Variables And Concatenation Challenge ==
+===========================================
+
+[1] Create 3 Variables [Title, Desctiption, Date]
+-- All In One Statement
+-- Variable Name Must Be Two Words
+-- Title Content Is "Elzero"
+-- Description Content Is "Elzero Web School"
+-- Date Content Is "25/10"
+[2] Create Variable Contains Div And This Div Contains
+-- H3 For Title
+-- P For Paragraph
+-- Span For Time
+[3] Add This Card To Page 4 Times
+[4] Use Template Literals For Concatenate
+
+Extra
+- Use ES6 Repeat
+*/
